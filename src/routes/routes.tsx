48eb786394ebdb86e604e6f1b2b773ae387c2e19
johnLamberts <faithsacredoo3@gmail.com>
2024-04-14 14:29:28 +0800

    fix/feat: add login route and improve routes for homepage

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,29 @@
+import Login from "@/modules/authentication/login-page/login";
 import GeneralError from "@/modules/error-pages/general-error";
 import NotFoundError from "@/modules/error-pages/not-found";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
+  { path: "/login", Component: Login },
   {
-    index: true,
+    path: "/",
     lazy: async () => ({
-      Component: (await import("@/modules/home-page/home-page")).default,
+      Component: (await import("@/layouts/HomeLayout")).default,
     }),
+    children: [
+      {
+        index: true,
+        lazy: async () => ({
+          Component: (await import("@/modules/home-page/home-page")).default,
+        }),
+      },
+      {
+        path: "/home",
+        lazy: async () => ({
+          Component: (await import("@/modules/home-page/home-page")).default,
+        }),
+      },
+    ],
   },
 
   { path: "/500", Component: GeneralError },
