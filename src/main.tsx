7ef4b791de031777bb48d5fd18b28631252d5e90
johnLamberts <faithsacredoo3@gmail.com>
2024-04-14 17:05:05 +0800

    feat: inject the setupcompoennt tot hte main

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "@/routes/routes";
+import SetupComponents from "./components/setup-components";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <SetupComponents>
+      <RouterProvider router={router} />
+    </SetupComponents>
   </React.StrictMode>
 );
