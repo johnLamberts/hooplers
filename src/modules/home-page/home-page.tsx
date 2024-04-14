b7f590b583cba97bd74016c6f027ca49b603b785
johnLamberts <faithsacredoo3@gmail.com>
2024-04-14 10:21:11 +0800

    committing

--- src/modules/home-page/home-page.tsx
+++ src/modules/home-page/home-page.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { SVGProps } from "react";
 import { Link } from "react-router-dom";
 import { JSX } from "react/jsx-runtime";
