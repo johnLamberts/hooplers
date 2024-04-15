cbbb3607275b7797715b0af3610392f3b22c23d2
johnLamberts <faithsacredoo3@gmail.com>
2024-04-15 21:41:40 +0800

    feat: add posts compoennts

---
+++ src/modules/posts-page/features/create-posts.tsx
@@ -0,0 +1,13 @@
+import { Layout, LayoutBody } from "@/components/layouts";
+import Tiptap from "./tiptap";
+
+const CreatePosts = () => {
+  return (
+    <Layout>
+      <LayoutBody>
+        <Tiptap />
+      </LayoutBody>
+    </Layout>
+  );
+};
+export default CreatePosts;
